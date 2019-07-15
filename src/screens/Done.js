ec2d768b63c19e611ec13341c74dd76304ddedd2
onurburakakin <onurburakakinn@gmail.com>
2019-07-15 18:57:31 +0300

    done and tasks lists

--- src/screens/Done.js
+++ src/screens/Done.js
@@ -1,66 +1,73 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, FlatList } from 'react-native';
-import { Text } from 'native-base';
+import { Text,Card,CardItem } from 'native-base';
 import axios from 'axios';
 
 export default class Done extends Component {
-  constructor(props) {
-    super(props);
-    this.token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTkyLjE2OC4xLjI5OjgwMDAvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE1NjI5MTgzMjEsImV4cCI6MTU2MjkyMTkyMSwibmJmIjoxNTYyOTE4MzIxLCJqdGkiOiJUTHExWVBhVlZRRGRXNTJJIiwic3ViIjoxLCJwcnYiOiI4N2UwYWYxZWY5ZmQxNTgxMmZkZWM5NzE1M2ExNGUwYjA0NzU0NmFhIn0.LnDvPHWJUH55_WWTMH5T0GHKGpd4B5yK8IZW2CaxswY';
-    this.state = {
-      dataSource: [],
-      offset: 0,
-      limit: 5
-    }
-  }
-  getData = () => {
-    const config = {
-      headers: {
-        'Authorization': "bearer " + this.token,
-        'Accept': 'application/json'
-      }
-    }
-     
-    const response = axios.get('http://192.168.1.29:8000/api/todolist?offset=' + this.state.offset + '&limit=' +
-      this.state.limit, config
-    ).then(response => {
-      console.log(response)
-      this.setState({
-        data: { ...this.state.data, ...response.data.results }
-      });
-      console.log(this.state)
-    }).catch(error => {
-      console.log(error)
-    })
-  }
-  componentDidMount() {
-    this.getData(0, 10)
-  }
-  renderItem = ({ item }) => {
-    return (
-      <View>
-        <Text>
-          {item.title}
-        </Text>
-      </View>
-    )
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <FlatList
-          data={this.state.dataSource}
-          renderItem={this.renderItem}
-        />
-      </View>
-    );
-  }
+	constructor(props) {
+		super(props)
+		this.token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTkyLjE2OC4xLjI5L2FwaS9hdXRoL2xvZ2luIiwiaWF0IjoxNTYzMDkxMDkxLCJleHAiOjE1NjMwOTQ2OTEsIm5iZiI6MTU2MzA5MTA5MSwianRpIjoiTlFXaXNzTENXZlVuMnJGaiIsInN1YiI6MiwicHJ2IjoiODdlMGFmMWVmOWZkMTU4MTJmZGVjOTcxNTNhMTRlMGIwNDc1NDZhYSJ9.kUZa1neQrBx43JvxZpwBcbeHNHp1rKiBb5agWJua1mY';
+		this.state = {
+			data: [],
+			offset: 0,
+			limit: 5
+		}
+	}
+	getData = () => {
+		const config = {
+			headers: {
+				'Authorization': "bearer " + this.token,
+				'Accept': 'application/json'
+			}
+		}
+
+		axios.get('http://172.20.10.4:8008/api/todolist/done?offset=' + this.state.offset + '&limit=' + this.state.limit, config
+		).then(response => {
+			console.log(response)
+			this.setState({
+				data: [...this.state.data, ...response.data.results],
+				offset: this.state.data.length
+
+			});
+			console.log(this.state)
+		}).catch(error => {
+			console.log(error)
+		})
+	}
+	componentDidMount() {
+		this.getData(0, 10)
+	}
+	renderItem = ({ item }) => {
+		return (
+			<View>
+				<Card>
+					<CardItem>
+						<Text>
+							{item.title}
+						</Text>
+					</CardItem>
+				</Card>
+			</View>
+		)
+	}
+	render() {
+		return (
+			<View style={styles.container}>
+				<FlatList
+					data={this.state.data}
+					renderItem={this.renderItem}
+					onEndReached={this.getData}
+					onEndReachedThreshold={0.5}
+				/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    textAlign: "center"
-  }
+	container: {
+		flex: 1,
+		flexDirection: 'row',
+		textAlign: "center"
+	}
 }); 
--- src/screens/Tasks.js
+++ src/screens/Tasks.js
@@ -1,34 +1,82 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Container, Text, Icon, Fab, Button } from 'native-base'; 
- 
+import { StyleSheet, View, FlatList } from 'react-native';
+import { Container, Icon, Fab, Card, Text, CardItem, Content, Left, Right, ListView } from 'native-base';
+import axios from 'axios';
+
 
 
 export default class Tasks extends Component {
-  render() {
-    return (
-      <Container>
-        <View style={styles.container}> 
-          <Fab 
-            style={{ backgroundColor: '#5067FF', flex: 3 }}
-            position="bottomRight">
-            <Icon name="plus"/>
-          </Fab>
-        </View>
-      </Container>
-    );
-  }
+	constructor(props) {
+		super(props)
+		this.token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTkyLjE2OC4xLjI5L2FwaS9hdXRoL2xvZ2luIiwiaWF0IjoxNTYzMDkxMDkxLCJleHAiOjE1NjMwOTQ2OTEsIm5iZiI6MTU2MzA5MTA5MSwianRpIjoiTlFXaXNzTENXZlVuMnJGaiIsInN1YiI6MiwicHJ2IjoiODdlMGFmMWVmOWZkMTU4MTJmZGVjOTcxNTNhMTRlMGIwNDc1NDZhYSJ9.kUZa1neQrBx43JvxZpwBcbeHNHp1rKiBb5agWJua1mY';
+		this.state = {
+			data: [],
+			offset: 0,
+			limit: 5
+		}
+	}
+	getData = () => {
+		const config = {
+			headers: {
+				'Authorization': "bearer " + this.token,
+				'Accept': 'application/json'
+			}
+		}
+
+		axios.get('http://192.168.1.29:8008/api/todolist/tasks?offset=' + this.state.offset + '&limit=' + this.state.limit, config
+		).then(response => {
+			console.log(response)
+			this.setState({
+				data: [...this.state.data, ...response.data.results],
+				offset: this.state.data.length
+
+			});
+			console.log(this.state)
+		}).catch(error => {
+			console.log(error)
+		})
+	}
+	componentDidMount() {
+		this.getData(0, 10)
+	}
+	renderItem = ({ item }) => {
+		return (
+			<View>
+				<Card>
+					<CardItem>
+						<Text>
+							{item.title}
+						</Text>
+					</CardItem>
+				</Card>
+			</View>
+		)
+	}
+	render() {
+		return (
+			<Container>
+				<View style={styles.container}>
+					<Fab
+						style={{ backgroundColor: '#5067FF', flex: 3 }}
+						position="bottomRight">
+						<Icon name="plus" />
+					</Fab>
+				</View>
+			</Container>
+		);
+
+	}
 }
- 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row'
-  },
-  tasks_title: {
-    color: 'red',
-    textAlign: "center",
-    marginTop: 20,
-  },
+	container: {
+		flex: 1,
+		flexDirection: 'row'
+	},
+	tasks_title: {
+		color: 'red',
+		textAlign: "center",
+		marginTop: 20,
+	},
 
 }); 
